0e66cd65b879630fae3925e5d484044b1cf108dc
Mbegu00 <33913705+Mbegu00@users.noreply.github.com>
2023-07-19 22:24:39 -0400

    Update script.js
    Mejoras, error solucionado ya muestra mensaje de error si es que no ingresas la cantidad de letras requerida

--- script.js
+++ script.js
@@ -24,38 +24,42 @@ fetch(UrlApi).then(response => response.json())
 function intentar() {
     let intento = document.getElementById('guess-input').value
     CONTENEDOR.innerHTML = ``
-    if(intento != 0){
-        const INTENTO = intento.toUpperCase()
-        const GRID = document.getElementById('cuadro')
-        const ROW = document.createElement('div')
-        ROW.className = 'row'
-        for (let i in palabra){
-            const SPAN = document.createElement('span')
-            SPAN.className = 'letter'
-            SPAN.innerHTML = INTENTO[i]
-            if(INTENTO[i] == palabra[i]){
-                SPAN.style.backgroundColor = "#79b851"
-            }else if(palabra.includes(INTENTO[i])){
-                SPAN.style.backgroundColor = "yellow"
-            }else{
-                SPAN.style.backgroundColor = "gray"
+    if(5 == intento.length){
+        if(intento != 0){
+            const INTENTO = intento.toUpperCase()
+            const GRID = document.getElementById('cuadro')
+            const ROW = document.createElement('div')
+            ROW.className = 'row'
+            for (let i in palabra){
+                const SPAN = document.createElement('span')
+                SPAN.className = 'letter'
+                SPAN.innerHTML = INTENTO[i]
+                if(INTENTO[i] == palabra[i]){
+                    SPAN.style.backgroundColor = "#79b851"
+                }else if(palabra.includes(INTENTO[i])){
+                    SPAN.style.backgroundColor = "yellow"
+                }else{
+                    SPAN.style.backgroundColor = "gray"
+                }
+                GRID.appendChild(SPAN)
             }
-            GRID.appendChild(SPAN)
+            GRID.appendChild(ROW)
+    
+            intentos--
+            if(INTENTO === palabra){
+                final('<h1>Ganaste</h1>')
+                return
+            }
+            if(intentos == 0){
+                final('<h1>PERDISTE<h1>')
+            }
+    
+        }else{
+            
+            sinDatos('<h1>INGRESA UN DATO<h1>')
         }
-        GRID.appendChild(ROW)
-
-        intentos--
-        if(INTENTO === palabra){
-            final('<h1>Ganaste</h1>')
-            return
-        }
-        if(intentos == 0){
-            final('<h1>PERDISTE<h1>')
-        }
-
     }else{
-        
-        sinDatos('<h1>INGRESA UN DATO<h1>')
+        sinDatos('<h1>INGRESA SI O SI 5 LETRAS<h1>')
     }
     
 }
